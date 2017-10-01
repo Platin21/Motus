7b8b91f1e2d51c17a1f76a687a9017e29415d103
Platin21 <arminmops@icloud.com>
2017-10-01 23:28:03 +0200

    Tiny shared ptr maybe we add control block but im not sure if it bloats

--- core/main.cpp
+++ core/main.cpp
@@ -1,15 +1,65 @@
-//
-//  main.cpp
-//  core
-//
-//  Created by Armin Hamar on 01.10.17.
-//  Copyright © 2017 Armin Hamar. All rights reserved.
-//
+#include <cstdlib>
+#include <cstdio>
+#include <memory>
 
-#include <iostream>
+using i64 = long long;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+template<typename T>
+class Shared
+{
+private:
+    T* ptr;
+    i64* refs;
+public:
+    T* ref() const
+    {
+        return ptr;
+    }
+    
+    i64 getRefCount() const
+    {
+        return *refs;
+    }
+    
+    Shared(T* raw_memory)
+    {
+        refs = (i64*)calloc(1,sizeof(i64));
+        ptr = raw_memory;
+    }
+    
+    Shared(Shared<T>&& shared)
+    {
+        refs = shared.refs;
+        *refs += 1;
+        ptr = shared.ptr;
+    }
+    
+    ~Shared()
+    {
+        if(*refs <= 0)
+        {
+            free(ptr);
+        }
+        else
+        {
+            *refs -= 1;
+        }
+    }
+};
+
+template<typename T>
+Shared<T> make_shared(i64 count)
+{
+    return Shared<T>( (T*)calloc(count,sizeof(T)) );
+}
+
+
+int main(int argc, const char * argv[])
+{
+ Shared<int> owner = make_shared<int>(30);
+ int* p = owner.ref();
+    
+ printf("%lu",sizeof(owner));
+    
+  return 0;
 }

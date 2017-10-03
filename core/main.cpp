590e61996215b5fc02ea7e7e468ac712e8dd57e4
Platin21 <arminmops@icloud.com>
2017-10-03 22:39:24 +0200

    tests

--- core/main.cpp
+++ core/main.cpp
@@ -2,64 +2,14 @@
 #include <cstdio>
 #include <memory>
 
+#include "Shared.hpp"
+
 using i64 = long long;
-
-template<typename T>
-class Shared
-{
-private:
-    T* ptr;
-    i64* refs;
-public:
-    T* ref() const
-    {
-        return ptr;
-    }
-    
-    i64 getRefCount() const
-    {
-        return *refs;
-    }
-    
-    Shared(T* raw_memory)
-    {
-        refs = (i64*)calloc(1,sizeof(i64));
-        ptr = raw_memory;
-    }
-    
-    Shared(Shared<T>&& shared)
-    {
-        refs = shared.refs;
-        *refs += 1;
-        ptr = shared.ptr;
-    }
-    
-    ~Shared()
-    {
-        if(*refs <= 0)
-        {
-            free(ptr);
-        }
-        else
-        {
-            *refs -= 1;
-        }
-    }
-};
-
-template<typename T>
-Shared<T> make_shared(i64 count)
-{
-    return Shared<T>( (T*)calloc(count,sizeof(T)) );
-}
-
 
 int main(int argc, const char * argv[])
 {
- Shared<int> owner = make_shared<int>(30);
- int* p = owner.ref();
-    
- printf("%lu",sizeof(owner));
+  Shared<int> owner = make_shared<int>({1,2,3,4,5,6});
+  int* p = owner.ref();
     
   return 0;
 }

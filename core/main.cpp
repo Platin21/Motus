900163a1b7f87933bdb71d433a283f10eb560483
Platin21 <arminmops@icloud.com>
2017-10-05 19:58:26 +0200

    minimal changes to make the project faster build

--- core/main.cpp
+++ core/main.cpp
@@ -1,8 +1,8 @@
-#include <cstdlib>
-#include <cstdio>
-#include <memory>
+#include <iostream>
 
-#include "Shared.hpp"
+#include "Shared.pch"
+#include "Range.pch"
+
 
 using i64 = long long;
 
@@ -10,6 +10,11 @@ int main(int argc, const char * argv[])
 {
   Shared<int> owner = make_shared<int>({1,2,3,4,5,6});
   int* p = owner.ref();
+    
+  Range<int> default_range = Range<int>(p,6);
+    
+  default_range.move_forward(1);
+  std::cout << default_range.peek() << " " << default_range.get_next() << "\n";
     
   return 0;
 }

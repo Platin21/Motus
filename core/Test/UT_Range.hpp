19bb67791db87ba6350730e450235c9ad0ca403a
Platin21 <arminmops@icloud.com>
2017-10-07 18:09:19 +0200

    Add the test cases

--- core/Test/UT_Range.hpp
+++ core/Test/UT_Range.hpp
@@ -8,9 +8,38 @@
 #undef TEST
 #define TEST(METHOD) if(!METHOD())
 
+// Add the Other tests
+
+bool UT_Range_init()
+{
+    int it_rng[5]{1};
+    auto rng = Range<int>(&it_rng[0],5);
+    return true;
+}
+
+bool UT_Range_peek()
+{
+    int it_rng[] = {1,2,3,4,5};
+    auto rng = Range<int>(&it_rng[0],5);
+    if(rng.peek() == 2)
+    {
+        return true;
+    };
+    
+    return false;
+}
+
 void UT_Range()
 {
+    TEST(UT_Range_init)
+    {
+        assert(false);
+    }
     
+    TEST(UT_Range_peek)
+    {
+        assert(false);
+    }
 }
 
 #endif

1c31bfbf6de934ff3b68ee3518d6bd38f69689fb
Platin21 <arminmops@icloud.com>
2017-10-07 15:50:31 +0200

    Add ifdefs also add MT prefix by version

--- core/Test/UT_Range.hpp
+++ core/Test/UT_Range.hpp
@@ -3,3 +3,10 @@
 // @n Motus_Lib
 // @a Armin Hamar
 // @l https://www.binpress.com/license/view/l/dc100faf74fa8ba3d613f8255ae1ef73
+#include "Range.pch"
+#ifdef MT_VERSION_1_2_0
+#undef TEST
+#define TEST(METHOD) if(!METHOD())
+
+
+#endif

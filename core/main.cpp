11db9d0c5e9963d9300c58e7da23c54e12521829
Platin21 <arminmops@icloud.com>
2017-10-13 22:00:15 +0200

    Add a simple unittest to main will move this to the test classes

--- core/main.cpp
+++ core/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
+#include "Types.pch"
 #include "Shared.pch"
 #include "Range.pch"
 #include "Unique.pch"
-#include "Types.pch"
-
-using i64 = long long;
+#include "SizeOf.pch"
 
 int main(int argc, const char * argv[])
 {
+    i64 number = 0;
+    printf("Typein number to count decimals:");
+    scanf("%lld",&number);
+    i64 count = CountOfDecimalCharsIn_i64(number);
+    printf("%lld\n",count);
     
     return 0;
 }

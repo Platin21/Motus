25dab5b4e2387dff29a332e5887ed6cf7f1bab17
Platin21 <arminmops@icloud.com>
2017-10-16 19:05:16 +0200

    Add simple file implementation

--- main.cpp
+++ main.cpp
@@ -8,28 +8,71 @@
 #include "Unique.pch"
 #include "Util.pch"
 #include "Convert.pch"
+#include "TextModifier.pch"
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+class File
+{
+    FileHandle   handle;
+    i64 file_size;
+public:
+    File(const Char* path)
+    {
+      handle = open(path,O_RDONLY);
+      if(handle)
+      {
+          struct stat info;
+          fstat(handle,&info);
+          file_size = info.st_size;
+      }
+      else
+      {
+          file_size = 0;
+          handle = 0;
+      }
+    }
+    
+    const i64 size()
+    {
+        return file_size;
+    }
+    
+    void readAll(Unique<char>& content)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,content.ref(),file_size);
+            content.ref()[file_size+1] = '\0';
+        }
+    }
+    
+    void readByte(Unique<char>& content)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,content.ref(),1);
+            content.ref()[2] = '\0';
+        }
+    }
+    
+    ~File()
+    {
+        close(handle);
+    }
+};
+
+struct vec3{ f32 x,y,z; };
 
 int main(int argc, const char * argv[])
 {
-    if(FileHandle file = open("./test.txt",O_RDONLY))
-    {
-        struct stat file_info;
-        fstat(file,&file_info);
-        auto file_content = make_unique<char>(file_info.st_size);
-        read(file,file_content.ref(),file_info.st_size);
-    }
-    f32 d = -3434.3452;
-    char converted[16];
-    decimal_to_cstr(d,&converted[0]);
+    File f = File("/Users/arminhamar/Dokumente/C++/core/Data/test.txt");
     
-    f32 rd = 0.0f;
-    charptr_to_decimal_value(&converted[0],&rd);
+    auto data = make_unique<char>(f.size()+1);
+    f.readAll(data);
     
     return 0;
 }

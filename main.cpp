776f7cdaca234010bf0f9359a6236e7f47205383
Platin21 <arminmops@icloud.com>
2017-10-19 21:06:21 +0200

    Minimal changes for utf8 and because of file read.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "Util.hpp"
 #include "Convert.hpp"
 #include "TextModifier.hpp"
+#include "CString.hpp"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,9 +21,9 @@ class File
     FileHandle   handle;
     i64 file_size;
 public:
-    File(const Char* path)
+    File(const utf8Char* path)
     {
-      handle = open(path,O_RDONLY);
+      handle = open(Char(path),O_RDONLY);
       if(handle)
       {
           struct stat info;
@@ -36,28 +37,87 @@ public:
       }
     }
     
-    const i64 size()
+    const i64& size() const
     {
         return file_size;
     }
     
-    void readAll(Unique<char>& content)
+    void safelyReadContent(Unique<utf8Char>& to)
     {
         if(handle != 0 && file_size != 0)
         {
-            read(handle,content.ref(),file_size);
-            content.ref()[file_size+1] = '\0';
+            read(handle,to.ref(),size());
+            to.ref()[size()+1] = '\0';
         }
     }
     
-    void readByte(Unique<char>& content)
+    void safelyReadByte(Unique<utf8Char>& to)
     {
         if(handle != 0 && file_size != 0)
         {
-            read(handle,content.ref(),1);
-            content.ref()[2] = '\0';
+            read(handle,to.ref(),1);
+            to.ref()[2] = '\0';
+            file_size -= 1;
         }
     }
+    
+    void readContent(Unique<utf8Char>& to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to.ref(),size());
+            file_size = 0;
+        }
+    }
+    
+    void readByte(Unique<utf8Char>& to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to.ref(),1);
+            file_size -= 1;
+        }
+    }
+    
+    //
+    void safelyReadContent(utf8Char* to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to,size());
+            to[size()+1] = '\0';
+            file_size = 0;
+        }
+    }
+    
+    void safelyReadByte(utf8Char* to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to,1);
+            to[2] = '\0';
+            file_size -= 1;
+        }
+    }
+    
+    void readContent(utf8Char* to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to,size());
+            file_size = 0;
+        }
+    }
+    
+    void readByte(utf8Char* to)
+    {
+        if(handle != 0 && file_size != 0)
+        {
+            read(handle,to,1);
+            file_size -= 1;
+        }
+    }
+    //
     
     ~File()
     {
@@ -69,10 +129,10 @@ struct vec3{ f32 x,y,z; };
 
 int main(int argc, const char * argv[])
 {
-    File f = File("/Users/arminhamar/Dokumente/C++/core/Data/test.txt");
+    File f = File(UTF8("/Users/arminhamar/Dokumente/C++/core/Data/käse.txt"));
     
-    auto data = make_unique<char>(f.size()+1);
-    f.readAll(data);
+    auto data = make_unique<utf8Char>(f.size()+1);
+    f.readContent(data.ref());
     
     return 0;
 }

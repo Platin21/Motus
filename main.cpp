fb7d907bc1d8006c90041156f3ada4b639f5986e
Platin21 <arminmops@icloud.com>
2017-10-20 22:26:46 +0200

    Created an dir structure

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <memory>
 
 //#define MT_SPECIAL_DEF
 
@@ -13,21 +12,60 @@
 #include "CString.hpp"
 #include "File.hpp"
 
-struct vec3{ f32 x,y,z; };
+#include <dirent.h>
 
-class z
+class Directory
 {
+private:
+    DIR *dir;
+    struct dirent *dent;
 public:
-    std::unique_ptr<utf8Char> ptr;
+    Directory(utf8Char* path)
+    {
+        dir = opendir(Char(path));
+        readdir(dir);
+        readdir(dir);
+        
+        if(dir == nullptr) return;
+    }
+    
+    void getFileInDir(Unique<utf8Char>& unique)
+    {
+     dent = readdir(dir);
+     if(dent == nullptr) return;
+     i64 path_size = CStringLenghtOf(UTF8(dent->d_name));
+     CStringCopy(unique.ref(),UTF8(dent->d_name),path_size);
+    }
+    
+    DirHandle getDirHandle()
+    {
+        return dirfd(dir);
+    }
+    
+    ~Directory()
+    {
+        if(dir != nullptr) closedir(dir);
+    }
 };
 
 int main(int argc, const char * argv[])
 {
-    setlocale(LC_ALL,"de_DE");
-    File f = File(UTF8("/Users/arminhamar/Dokumente/C++/core/Data/käse.txt"),OpenForWrite);
-    utf8Char txt[] = "lol";
-    f.write(3,&txt[0]);
-
+    
+    File f = File(UTF8("/Users/arminhamar/Dokumente/C++/core/Data/lese.txt"),OpenForWrite);
+    utf8Char *t = UTF8("vec3(1.0,2.0,3.0)\n");
+    
+    f.write(t,CStringLenghtOf(t));
+    Directory d = Directory(UTF8("/Users/arminhamar/Dokumente/C++/core/Data/"));
+    
+    Unique<utf8Char> filename = make_unique<utf8Char>(30);
+    d.getFileInDir(filename);
+    printf("%s",filename.ref());
+    
+    File f2 = File(filename.ref(),d.getDirHandle(),OpenForRead);
+    auto content = make_unique<utf8Char>(f2.size());
+    f2.safelyReadContent(content);
+    
+    printf("\n%s\n",content.ref());
     return 0;
 }
 

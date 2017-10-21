d3569b78baa8fe153d9cd8ebb869db35b36d513e
Platin21 <arminmops@icloud.com>
2017-10-21 17:39:16 +0200

    Add new file for Directory class

---
+++ Base/Directory.hpp
@@ -0,0 +1,47 @@
+// @v 1.11.0
+// @n Motus Lib
+// @a Armin Hamar
+// @l https://www.binpress.com/license/view/l/dc100faf74fa8ba3d613f8255ae1ef73
+#pragma once
+#include "Types.hpp"
+#include "Unique.hpp"
+
+// Header
+#define MT_VERSION_1_11_0
+// Header
+
+#include <dirent.h>
+
+class Directory
+{
+private:
+    DIR *dir;
+    struct dirent *dent;
+public:
+    Directory(utf8Char* path)
+    {
+        dir = opendir(Char(path));
+        readdir(dir);
+        readdir(dir);
+        
+        if(dir == nullptr) return;
+    }
+    
+    void getFileInDir(Unique<utf8Char>& unique)
+    {
+        dent = readdir(dir);
+        if(dent == nullptr) return;
+        i64 path_size = CStringLenghtOf(UTF8(dent->d_name));
+        CStringCopy(unique.ref(),UTF8(dent->d_name),path_size);
+    }
+    
+    DirHandle getDirHandle()
+    {
+        return dirfd(dir);
+    }
+    
+    ~Directory()
+    {
+        if(dir != nullptr) closedir(dir);
+    }
+};

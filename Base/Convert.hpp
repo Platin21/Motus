1a58870dc0815f7147773170a2d312b1107d5317
Platin21 <arminmops@icloud.com>
2017-10-21 17:39:30 +0200

    Add new functions to convert

--- Base/Convert.hpp
+++ Base/Convert.hpp
@@ -168,3 +168,56 @@ void charptr_to_decimal_value(const utf8Char *txt,T *out) {
     }
     *out += part;
 }
+
+i32 hexCharToDecimal(utf8Char& hexChar)
+{
+    utf8Char c = hexChar;
+    if(c <= 97 && c >= 65) c = hexChar + 32;
+    
+    switch(c)
+    {
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        case 'a':
+            return 10;
+        case 'b':
+            return 11;
+        case 'c':
+            return 12;
+        case 'd':
+            return 13;
+        case 'e':
+            return 14;
+        case 'f':
+            return 15;
+    }
+    return 0;
+}
+
+i32 hexToDecimal(utf8Char* hex,i64 lenght)
+{
+    i32 sumOfHex = 0;
+    for(i64 i = 0,b = lenght-1;i < lenght; ++i,--b)
+    {
+        sumOfHex += (hexCharToDecimal(hex[i]) * powerOf16(b));
+    }
+    return sumOfHex;
+}

2f19b6d2863937204f4efb2ca92616dae81fb8f2
Platin21 <arminmops@icloud.com>
2017-10-21 17:41:21 +0200

    Some changes in the include section and some minimal tests with the structs

--- main.cpp
+++ main.cpp
@@ -1,119 +1,19 @@
 #include <iostream>
 
 //#define MT_SPECIAL_DEF
-
+//@CORE
 #include "Types.hpp"
 #include "Shared.hpp"
 #include "Range.hpp"
 #include "Unique.hpp"
 #include "Util.hpp"
+#include "CString.hpp"
+//@BASE
 #include "Convert.hpp"
 #include "TextModifier.hpp"
-#include "CString.hpp"
 #include "File.hpp"
-
-#include <dirent.h>
-
-class Directory
-{
-private:
-    DIR *dir;
-    struct dirent *dent;
-public:
-    Directory(utf8Char* path)
-    {
-        dir = opendir(Char(path));
-        readdir(dir);
-        readdir(dir);
-        
-        if(dir == nullptr) return;
-    }
-    
-    void getFileInDir(Unique<utf8Char>& unique)
-    {
-     dent = readdir(dir);
-     if(dent == nullptr) return;
-     i64 path_size = CStringLenghtOf(UTF8(dent->d_name));
-     CStringCopy(unique.ref(),UTF8(dent->d_name),path_size);
-    }
-    
-    DirHandle getDirHandle()
-    {
-        return dirfd(dir);
-    }
-    
-    ~Directory()
-    {
-        if(dir != nullptr) closedir(dir);
-    }
-};
-
-i16 hexCharToDecimal(utf8Char& c)
-{
-    switch(c)
-    {
-      case 48:
-        return 0;
-      case 49:
-        return 1;
-      case 50:
-        return 2;
-      case 51:
-        return 3;
-      case 52:
-        return 4;
-      case 53:
-        return 5;
-      case 54:
-        return 2;
-      case 55:
-        return 3;
-      case 56:
-        return 4;
-      case 57:
-        return 5;
-      case 58:
-        return 4;
-      case 59:
-        return 5;
-      case 60:
-        return 6;
-      case 61:
-        return 7;
-      case 62:
-        return 8;
-      case 63:
-        return 9;
-      case 'a':
-        return 10;
-      case 'b':
-        return 11;
-      case 'c':
-        return 12;
-      case 'd':
-        return 13;
-      case 'e':
-        return 14;
-      case 'f':
-        return 15;
-      default:
-        return 0;
-    }
-}
-
-class Color
-{
-    short Red,Green,Blue;
-public:
-    Color(short r,short g,short b): Red(r),Green(g),Blue(b) {};
-    Color(utf8Char* RGBhexColor)
-    {
-        u32 hexColor = 0;
-        Red   = ((hexColor >> 16) & 0xFF) / 255.0;  // get red
-        Green = ((hexColor >> 8) & 0xFF) / 255.0;   // get green
-        Blue  = ((hexColor) & 0xFF) / 255.0;        // get blue
-    }
-};
+#include "Color.hpp"
+#include "Directory.hpp"
 
 int main(int argc, const char * argv[])
 {
@@ -134,6 +34,10 @@ int main(int argc, const char * argv[])
     f2.safelyReadContent(content);
     
     printf("\n%s\n",content.ref());
+    
+    Color c = UTF8("#4C99B2FA");
+    c.Negativ();
+    
     return 0;
 }
 
